3554a1e8280ed41e21c0b80119ff65ba07332d40
luu-jennifer <jenniferluu87@gmail.com>
2022-10-01 15:24:26 -0700

    pseudocode completed

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ zeldaApp.getInfo=()=>{
 
 //Click a button “Start Game” to display question section
     // event listener for the submitted value that has been selected in drop down menu (option)
-        // create variable for selected value
-        // create variable method for random generator Math.floor(Math.random( value * value.length))
+        // create variable for selected value = const selectValue
+        // create variable method for  const random Generator [Math.floor(Math.random( 10 * selectedValue.length))]
         // add in prevent default of default event on clicking "Show me" button
         // if user selects "food"
         // then random generate from food array (36 items) = food.length
@@ -43,29 +43,41 @@ zeldaApp.getInfo=()=>{
         // random generate a value from the answer from the array
         // then alert "select a preference"
 // 2: CREATE A NEW OBJECT
-        // creat a new empty obj named newObj
-        // grab the content in the food & non_food arraies
+        // create a new empty obj named newObj
+        // grab the content in the food & non_food arrays
         // loop through newObj
         // random generate a value from the answer from the newObj
         
     
-        // else user selects a submit value as an empty value "pick you preference" because an answer is required
+    // else user selects a submit value as an empty value "pick you preference" because an answer is required
+
+    //create a variable to store the creature index that was randomly generated
+        //const randomIndex
+
+    // Store randomly generator creature in a variable 
+        // const creatureHero = data.food[randomIndex]
 
 //When users finish the question, click a button “Show me” then display answer section
 
     // event listener for the show me button
-    // match selected value with object data's key name(food & non_food &combination of foor & non_food)
+    // match selected value with object data's key name(food & non_food &combination of food & non_food)
     // loop through food, non_food and the combination 
-    // create 3 <p> tags inside cardContentContainer(ul>li>p) with seperate class name: name, common_location and description;  and 1 <img> tag inside imgContainer with class name of image
+    // create 3 <p> tags inside cardContentContainer(ul>li>p) with separate class name: name, common_location and description;  and 1 <img> tag inside imgContainer with class name of image
 
 // Display card with:
-// Img: append a randomly generated image
-// Name
-// Location 
-// description  
+    // Img: append a creatureHero.image to imgContainer
+        // <img src ="" alt="name">
+    // Name: append a creatureHero.name 
+        // .name
+    // Location 
+        // append a creatureHero.common_locations
+            // .location
+    // description 
+        // append a creatureHero.description
+            // .description
 
 
-//init menthod
+//init method
 zeldaApp.init = ()=>{
     zeldaApp.getInfo()
 }
